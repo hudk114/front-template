31d3b718d7e0922833e731e78b93ed39877993cb
dekaihu <dekaihu@sohu-inc.com>
2019-11-04 00:17:49 +0800

    upd8 config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
   extends: 'standard',
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
     'linebreak-style': [2, 'unix'],
     semi: [2, 'always'],
     'no-console': 'off',
     'standard/no-callback-literal': 'off',
-    'one-var': 'off'
+    'one-var': 'off',
+    'space-before-function-paren': 'off',
   }
 };
